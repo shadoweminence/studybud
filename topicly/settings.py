6b605a420f20c0ce069c10741fafad983f842d01
Prashant Pokhrel <pokhrelprashant9@gmail.com>
2026-02-05 14:37:39 +0545

    fix: changed project name

--- topicly/settings.py
+++ topicly/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--**w)i500=($_$l+$(2q3@jic!f_uog=)lnf^#h776c3c^!mzi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "api.mysite.com",
